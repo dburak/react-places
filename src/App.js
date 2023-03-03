b57c136765ffb18cd2843614e1af32e2d3c3922b
burakdiker <burak.diker@ug.bilkent.edu.tr>
2023-03-04 01:16:51 +0300

    header, nav, user item components created

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
-function App() {
-  return <h1>Let's start</h1>
-}
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlace';
+import MainNavigation from './shared/components/Navigation/MainNavigation';
+
+const App = () => {
+  return (
+    <Router>
+      <MainNavigation />
+      <main>
+        <Switch>
+          <Route path='/' exact>
+            <Users />
+          </Route>
+          <Route path='/places/new' exact>
+            <NewPlace />
+          </Route>
+          <Redirect to='/' />
+        </Switch>
+      </main>
+    </Router>
+  );
+};
 
 export default App;
